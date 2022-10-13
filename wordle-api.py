8ce2e953c98b4e22a1adb950045d245bb566652e
tish978 <satish.bisa@gmail.com>
2022-10-13 14:43:37 -0700

    Created/Updated all tables with normalized columns.

--- wordle-api.py
+++ wordle-api.py
@@ -1,12 +1,14 @@
 from quart import Quart, request, jsonify
 from quart_schema import QuartSchema #, DataSource, validate_request
 import sqlalchemy
-from sqlalchemy import create_engine, insert, select, Table, Column, Integer, String
+from sqlalchemy import create_engine, insert, select, Table, Column, Integer, String, Boolean
 from databases import Database
 import socket
 from dataclasses import dataclass
 from flask import render_template, abort
 
+
+engine = create_engine('sqlite:///C:/Users/bruht/PycharmProjects/quart-test/testDB.db', echo=True)
 
 db = Database('sqlite:///C:/Users/bruht/PycharmProjects/quart-test/testDB.db')
 metadata = sqlalchemy.MetaData()
@@ -15,8 +17,32 @@ users = sqlalchemy.Table(
     "users",
     metadata,
     Column("user_id", Integer, primary_key=True),
-    Column("password", String(length=100))
+    Column("password", String(length=100)),
+    Column("games_played", Integer)
 )
+
+
+games = sqlalchemy.Table(
+    "games",
+    metadata,
+    Column("game_id", Integer, primary_key=True),
+    Column("game_secret_word", String(length=100)),
+    Column("won", Boolean),
+    Column("number_of_guesses_made", Integer),
+    Column("number_of_guesses_left", Integer),
+    Column("user_id", Integer, foreign_key=True)
+)
+
+
+wordGuess = sqlalchemy.Table(
+    "wordGuess",
+    metadata,
+    Column("user_word_guess_id", Integer, primary_key=True),
+    Column("user_word_guess", String(length=100)),
+    Column("game_id", Integer, foreign_key=True)
+)
+
+metadata.create_all(engine)
 
 
 app = Quart(__name__)
